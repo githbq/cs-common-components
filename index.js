bbabcd62f477d4eb1506c54654a8e1414c334b58
hubq <hubq@firstshare.cn>
2016-09-02 20:21:01 +0800

    dep

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var deps = ['mwl.confirm','ngSanitize'];
+deps = deps.concat(['ui.router', 'ui.layout', 'ui.bootstrap', 'oc.lazyLoad', 'common.frame']);
+deps = deps.concat(['ui.grid', 'ui.grid.selection', 'ui.grid.moveColumns', 'ui.grid.autoResize', 'ui.grid.pinning', 'ui.grid.resizeColumns', 'ui.grid.cellNav', 'ui.grid.pagination']);
+
+angular.module('common.directives', deps);
+angular.module('common.services', deps);
+angular.module('common.filters', deps); 
 
 require('./directives/index');
 require('./services/index');
