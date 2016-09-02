9fe85e23bb78fb8609c34a4af884b393d8dec7a3
hubq <hubq@firstshare.cn>
2016-09-02 20:24:29 +0800

    '减依赖'

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var deps = ['mwl.confirm','ngSanitize'];
-deps = deps.concat(['ui.router', 'ui.layout', 'ui.bootstrap', 'oc.lazyLoad', 'common.frame']);
+deps = deps.concat(['ui.router', 'ui.layout', 'ui.bootstrap']);
 deps = deps.concat(['ui.grid', 'ui.grid.selection', 'ui.grid.moveColumns', 'ui.grid.autoResize', 'ui.grid.pinning', 'ui.grid.resizeColumns', 'ui.grid.cellNav', 'ui.grid.pagination']);
 
 angular.module('common.directives', deps);
