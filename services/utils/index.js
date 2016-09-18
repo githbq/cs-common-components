60d570a95f26446e0fb18e262e81b6119f964a06
hubq <hubq@firstshare.cn>
2016-09-18 11:45:01 +0800

    ApiNoConfirm

---
+++ services/utils/index.js
@@ -0,0 +1,180 @@
+angular.module('common.components').factory('ibssUtils', function ($http, toaster) {
+    var factory = {
+        /**
+         * html 转义
+         */
+        html_encode: function (str) {
+            var s = "";
+            if (str.length == 0) {
+                return "";
+            }
+            s = str.replace(/</g, "&lt;");
+            s = s.replace(/>/g, "&gt;");
+            s = s.replace(/ /g, "&nbsp;");
+            s = s.replace(/\'/g, "&#39;");
+            s = s.replace(/\"/g, "&quot;");
+            s = s.replace(/\n/g, "<br>");
+            return s;
+        },
+
+        /**
+         * html 反转义
+         */
+        html_decode: function (str) {
+            var s = "";
+            if (str.length == 0) {
+                return "";
+            }
+            s = str.replace(/&lt;/g, "<");
+            s = s.replace(/&gt;/g, ">");
+            s = s.replace(/&nbsp;/g, " ");
+            s = s.replace(/&#39;/g, "\'");
+            s = s.replace(/&quot;/g, "\"");
+            s = s.replace(/<br>/g, "\n");
+            return s;
+        },
+
+        /**
+         * clone 一个对象
+         * @param obj {object}
+         */
+        clone: function (obj) {
+            return JSON.parse(JSON.stringify(obj));
+        },
+        //////////////
+        //
+        // 应用内跳转MAP
+        /////////////
+        getInAppName: function (key) {
+            var MAP = {
+                'tyzh': '体验帐号',
+                'bsxt': '报数系统',
+                'yqts': '邀请同事',
+                'grzl': '个人资料',
+                'xtsz': '系统设置',
+                'sccwrz': '立即上传'
+            };
+
+            if (key) {
+                return MAP[key];
+            } else {
+                return MAP;
+            }
+        },
+
+        /**
+         *
+         * 将以http开头的文字替换为超链接
+         */
+        replaceLink: function (content) {
+            content = content || '';
+            var HTTP_REG = new RegExp("((http[s]?|ftp)://|www\\.)[a-zA-Z0-9\\.\\-]+\\.([a-zA-Z]{2,4})(:\\d+)?(/[a-zA-Z0-9\\.\\-~!@#$%^&*+?:_/=<>]*)?", "gi");
+            return content.replace(HTTP_REG, function (c) {
+                return '<a target="_blank" href="' + c + '">' + c + '</a>';
+            });
+        },
+        ////////////////
+        //
+        //根据当前时间输出易读的时间
+        ////////////////
+        translateTime: function (time) {
+            debugger
+            if (!time) {
+                throw ('time 不能为空');
+            }
+            var now = new Date();
+            var datetime = new Date(time);
+
+            var str,
+                timeSpace;
+
+            if (now.getFullYear() != datetime.getFullYear()) {
+
+                str = "yyyy年MM月dd日 HH:mm";
+            } else {
+
+                if (now.getMonth() != datetime.getMonth()) {
+
+                    str = "MM月dd日 HH:mm";
+                } else {
+
+                    if (now.getDate() != datetime.getDate()) {
+                        now.setHours(0, 0, 0, 0);
+                        datetime.setHours(0, 0, 0, 0);
+                        timeSpace = now.getTime() - datetime.getTime();
+                        if (timeSpace <= (1000 * 60 * 60 * 24)) {
+                            return ("昨天 " + new Date(time)._format("HH:mm"));
+                        } else if ((timeSpace <= (1000 * 60 * 60 * 42))) {
+                            return ("前天 " + new Date(time)._format("HH:mm"));
+                        } else {
+                            str = "MM月dd日 HH:mm";
+                        }
+                    } else {
+
+                        str = "HH:mm";
+                    }
+                }
+            }
+
+            return new Date(time)._format(str);
+        },
+        getFileSize: function (byteSize) {
+            var v = 0, unit = "BYTE";
+            if (byteSize > 1073741824) {   //1G=1073741824 BYTE
+                v = (byteSize / 1073741824).toFixed(0);
+                unit = "GB";
+            } else if (byteSize > 1048576) {   //1M=1048576 BYTE
+                v = (byteSize / 1048576).toFixed(0);
+                unit = "MB";
+            } else if (byteSize > 1024) {
+                v = (byteSize / 1024).toFixed(0);
+                unit = "KB";
+            } else {
+                v = byteSize;
+                unit = "B";
+            }
+            return v + unit;
+        }
+    };
+    factory.api = function (opt) {
+        //默认设置
+        opt = _.extend({
+            method: 'POST',
+            cache: false,
+            timeout: 30000,
+            transformResponse: function (response, headers, status) {
+                if (status < 200 || status >= 300) {
+                    return;
+                }
+                var data = {};
+                try {
+                    data = JSON.parse(response);
+                } catch (ex) {
+                    toaster.pop('error', 'Response Error', 'Response json parsed failed.');
+                    return;
+                }
+                if (data.success != undefined && !data.success) {
+                    toaster.pop('error', 'Response Error', data.message);
+                    return;
+                }
+                return { success: data.success, model: data.value ? data.value.model : {} };
+            }
+        }, opt || {});
+        return $http(opt).error(function (data, status) {
+            toaster.pop('error', 'Request Failed', 'Status: ' + status);
+        });
+    };
+    factory.checkBlankSpace = function (str) {
+        while (str.lastIndexOf(" ") >= 0) {
+            str = str.replace(" ", "");
+        }
+        while (str.lastIndexOf("\n") >= 0) {
+            str = str.replace('\n', '');
+        }
+        if (str.length == 0) {
+            return false;
+        }
+        return true;
+    };
+    return factory;
+});
