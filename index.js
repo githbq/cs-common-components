14294b1f4f7c029ed0da17fc122223c1321b743e
liyl <liyl@fxiaoke.com>
2016-09-18 14:46:38 +0800

    * 增加confim

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('angular-ui-grid');
 require('angular-ui-grid/ui-grid.css');
 require('angular-drag');
-var deps = ['ui.grid', 'ui.grid.selection', 'angular-drag', 'ui.grid.moveColumns', 'ui.grid.autoResize', 'ui.grid.pinning', 'ui.grid.resizeColumns', 'ui.grid.cellNav', 'ui.grid.pagination', 'toaster'];
+require('angular-bootstrap-confirm');
+var deps = ['ui.grid', 'ui.grid.selection', 'angular-drag', 'ui.grid.moveColumns', 'ui.grid.autoResize', 
+'ui.grid.pinning', 'ui.grid.resizeColumns', 'ui.grid.cellNav', 'ui.grid.pagination', 'toaster','mwl.confirm'];
 module.exports = angular.module('common.components', deps).name;
 require('./directives');
 require('./filters');
