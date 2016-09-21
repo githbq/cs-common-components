899987ce24978c6f6c5895878a0585dc21a6c6e9
liyl <liyl@fxiaoke.com>
2016-09-21 14:25:01 +0800

    +

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require('angular-ui-grid');
 require('angular-ui-grid/ui-grid.css');
 require('angular-drag');
 require('angular-bootstrap-confirm');
+require('angular-i18n/angular-locale_zh-cn.js');
 var deps = ['ui.grid', 'ui.grid.selection', 'angular-drag', 'ui.grid.moveColumns', 'ui.grid.autoResize', 
-'ui.grid.pinning', 'ui.grid.resizeColumns', 'ui.grid.cellNav', 'ui.grid.pagination', 'toaster','mwl.confirm'];
+'ui.grid.pinning', 'ui.grid.resizeColumns', 'ui.grid.cellNav', 'ui.grid.pagination', 'toaster','mwl.confirm','ngLocale'];
 module.exports = angular.module('common.components', deps).name;
 require('./directives');
 require('./filters');
