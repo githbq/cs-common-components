593531ab822d55b2e468801ff657ce1f21a03cc7
liyl <liyl@fxiaoke.com>
2016-09-28 10:55:22 +0800

    * 修改bug

--- directives/permission/index.js
+++ directives/permission/index.js
@@ -1,17 +1,24 @@
 angular.module('common.components').directive('permission', function ($rootScope) {
     return {
         priority: 0,
-        controller:( $scope, $element, $attrs )=>{
-             //$rootScope.permissions=['1','2','3'];
-    
-            var codes = $attrs['permission'] = '' || $attrs['permission'].split(',');
-            var enabled = _.intersection($rootScope.modules, codes).length > 0;
-            if(!enabled){
-                $element.remove();
+        controller: ($scope, $element, $attrs) => {
+            //$rootScope.permissions=['1','2','3'];
+
+            $rootScope.$watch('modules', function (nv, ov) {
+                console.log('=== Modules changed: ' + nv);
+                $scope.onModulesChanged(nv, $element);
+            });
+            $scope.onModulesChanged = function (modules, $elem) {
+                var codes = $attrs.permission && $attrs.permission.split(',');
+                console.log('=== Scope permissons: ' + $attrs.permission);
+                var enabled = _.intersection(modules, codes).length > 0;
+                if (!enabled) {
+                    $elem.remove();
+                }
             }
         },
         link: ($scope, iElem, iAttr) => {
-           
+
         }
     };
 });
