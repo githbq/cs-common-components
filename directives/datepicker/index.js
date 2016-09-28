e0371a23922b0cff6ec3290cfe9e65d59e81c976
liyl <liyl@fxiaoke.com>
2016-09-28 13:37:51 +0800

    * 修改bug

--- directives/datepicker/index.js
+++ directives/datepicker/index.js
@@ -63,7 +63,7 @@ angular.module('common.components').directive('commonDatepicker', function ($tim
 
             //时间转化为秒
             $scope.$watch('innerModel', () => {
-                debugger
+                
                 if(_.isDate( $scope.innerModel ) ){
                      console.log($scope.innerModel)
                       $scope.ngModel =  $scope.innerModel ?  $scope.innerModel.getTime():'';
