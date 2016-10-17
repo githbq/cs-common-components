1f76ab7e62cc9ba8326d8e83073d72b6ca2c80a0
Peng Wan <wanpeng@fxiaoke.com>
2016-10-17 14:23:49 +0800

    merge revision cb271dd0309ac502d80bd0f2a44fb557400267cb

--- directives/grid/index.js
+++ directives/grid/index.js
@@ -1,14 +1,14 @@
 var templateStr = require('./index.html');
 require('./index.less');
-angular.module('common.components').directive('normalGrid', function ($timeout, $window) {
+angular.module('common.components').directive('normalGrid', function (toaster, $timeout, $window, $q) {
     return {
         restrict: 'CA',
         template: templateStr,
-        scope: { gridOptions: '=normalGrid', pageChange: '=' },
+        scope: { gridOptions: '=normalGrid' },
         link: function ($scope, iElem, iAttr) {
             $scope.showGrid = false;
             var defaultOptions = {
-                loading: false,//是否正处于加载状态 
+                loading: false,//是否正处于加载状态
                 enableFullRowSelection: true, //是否点击行任意位置后选中,默认为false,当为true时，checkbox可以显示但是不可选中
                 enableRowHeaderSelection: false, //是否显示选中checkbox框 ,默认为true
                 enableRowSelection: true, // 行选择是否可用，默认为true;
@@ -16,20 +16,22 @@ angular.module('common.components').directive('normalGrid', function ($timeout, 
                 enableSorting: false,
                 showGridFooter: false,
                 enableGridMenu: false,//是否显示表格最右上角的菜单
-                enableSelectionBatchEvent: true,//默认开启批量选中事件 此时全选复选框才有效果  
+                enableSelectionBatchEvent: true,//默认开启批量选中事件 此时全选复选框才有效果
                 enableFiltering: false,
                 paginationCurrentPage: 1,
                 paginationPageSizes: [10, 50, 75, 100],
                 paginationPageSize: 10,//分页默认数量
                 useExternalPagination: true,//是否使用远程ajax的分页这时候就知道写事件了 而不是表格自己进行假分页
-                useExternalSorting: false,//是否使用远程ajax的排序 
+                useExternalSorting: false,//是否使用远程ajax的排序
                 enablePagination: true, //是否分页，默认为true
                 enablePaginationControls: true,//使用默认的底部分页
                 showCustomPagination: false,//是否使用自定义的分页组件  默认不用
                 showIndexHeader: false,   //显示序列号
                 enableColumnMenu: false,//显示列的表头菜单
                 enableHorizontalScrollbar: 0, //grid水平滚动条是否显示, 0-不显示  1-显示
-                enableVerticalScrollbar: 1 //grid垂直滚动条是否显示, 0-不显示  1-显示
+                enableVerticalScrollbar: 1, //grid垂直滚动条是否显示, 0-不显示  1-显示,
+                autoPage: false,//是否启用默认封装的分页
+                searchModel: {}//搜索使用的模型 重写 以赋默认值   无需pageSize pageIndex字段
             };
             $scope.gridOptions = angular.extend({}, defaultOptions, $scope.gridOptions);
             if ($scope.gridOptions.showCustomPagination) {//如果使用自定义的分页则默认分页不启用
@@ -48,9 +50,17 @@ angular.module('common.components').directive('normalGrid', function ($timeout, 
             $scope.gridOptions._onRegisterApi = $scope.gridOptions.onRegisterApi;
             $scope.gridOptions.onRegisterApi = function (gridApi) {
                 $scope.gridOptions.gridApi = gridApi;
-                if ($scope.gridOptions.showIndexHeader) {
+                if ($scope.gridOptions.showIndexHeader) {//显示索引列
                     gridApi.core.addRowHeaderColumn({ name: '__sequence', displayName: '#', width: 50, cellTemplate: 'ui-grid/uiGridCell' });
                     gridApi.grid.registerRowsProcessor($scope.addIndexColumn, 200);
+                }
+                if ($scope.gridOptions.autoPage) {//开启默认封装的分页
+                    gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
+                        $scope.getPage({ pageIndex: newPage, pageSize: pageSize });
+                    });
+                    if ($scope.gridOptions.autoPage === 1) {//默认执行查询行为
+                        $scope.gridOptions.search();
+                    }
                 }
                 $scope.gridOptions._onRegisterApi && $scope.gridOptions._onRegisterApi(gridApi);
             };
@@ -65,7 +75,6 @@ angular.module('common.components').directive('normalGrid', function ($timeout, 
             function onResize() {
                 if (debounceEvent) $timeout.cancel(debounceEvent);
                 debounceEvent = $timeout(function () {
-
                     var parentContainer = iElem.parents('.ui-layout-container:first').length > 0 ? iElem.parents('.ui-layout-container:first') : iElem.parents('.ui-grid-dock-wrapper:first');
                     if (parentContainer.length > 0) {
                         var setHeight = parentContainer.height();
@@ -85,21 +94,83 @@ angular.module('common.components').directive('normalGrid', function ($timeout, 
             $scope.$on('$destroy', function () {
                 angular.element($window).unbind('resize', onResize);
             });
-            //end 容器大小改变处理  
+            //end 容器大小改变处理
+
+            //////////////////////分页
+            $scope.gridOptions.searchModel = $scope.gridOptions.searchModel || {};
+            //拉取数据的接口 使用时需要自行实现方法体
+            $scope.gridOptions.onPullData = $scope.gridOptions.onPullData || function () {
+                    var q = $q.defer();
+                    q.resolve({});
+                    return q.promise;
+                }
+            $scope.gridOptions.afterPullData = $scope.gridOptions.afterPullData || function (result) {
+                    if (result.data && result.data.success) {
+                        if (result.data.model.content.length == 0) {
+                            toaster.pop('info', null, '暂无数据', 1000);
+                        }
+                        $scope.searching = false;
+                        if ($scope.gridOptions.onSetData) {
+                            $scope.gridOptions.data = $scope.gridOptions.onSetData(result.data.model.content, result.data);
+                        } else {
+                            $scope.gridOptions.data = result.data.model.content;
+                        }
+                        $scope.gridOptions.totalItems = result.data.model.itemCount;
+                    }
+                }
+            //点击查询
+            $scope.gridOptions.search = function ($event) {
+                $scope.gridOptions.paginationCurrentPage = 1;
+                getPage($scope.gridOptions.searchModel);
+                $event && $event.stopPropagation();
+            }
+            var currentQueryData = _.extend({}, $scope.gridOptions.searchModel);//当前查询的数据   避免用户修改查询条件后未点击查询下 操作分页 数据查询异常BUG
+            function getPage(queryData) {
+                currentQueryData = angular.copy(queryData || currentQueryData);
+                $scope.gridOptions.loading = true;
+                $scope.searching = true;
+                //这里进行从后端拿数据赋值给 $scope.gridOptions.data操作
+                $scope.gridOptions.onPullData(_.extend({ pageSize: $scope.gridOptions.paginationPageSize, pageIndex: $scope.gridOptions.paginationCurrentPage }, currentQueryData))
+                    .then((result) => {
+                    $scope.gridOptions.afterPullData(result);
+            }, () => {
+                    $scope.gridOptions.data = [];
+                    $scope.gridOptions.totalItems = 0;
+                }).finally(() => {
+                    $scope.gridOptions.loading = false;
+            });
+            }
+            //////////////////////end 分页
         },
-        controller: function ($scope, uiGridConstants) {
-            $scope.$watch('gridOptions.paginationCurrentPage', function (newVal, oldVal) {
-                if ($scope.gridOptions.pageChange) {
-                    $scope.gridOptions.pageChange($scope.gridOptions.paginationPageSize, $scope.gridOptions.paginationCurrentPage);
+        controller: function ($scope, uiGridConstants, $templateCache) {
+            //////grid row 模板改造  支持双击事件
+            $templateCache.put('ui-grid/ui-grid-row/custom',
+                `<div ng-dblclick="grid.appScope.gridOptions.rowDoubleClick($event,row,this)"
+                 ng-repeat="(colRenderIndex, col) in colContainer.renderedColumns track by col.uid" 
+                 ui-grid-one-bind-id-grid="rowRenderIndex + '-' + col.uid + '-cell'" class="ui-grid-cell" 
+                 ng-class="{ 'ui-grid-row-header-cell': col.isRowHeader }" 
+                 role="{{col.isRowHeader ? 'rowheader' : 'gridcell'}}" ui-grid-cell> 
+                 </div>`
+            );
+            $scope.gridOptions.rowTemplate = $scope.gridOptions.rowTemplate || 'ui-grid/ui-grid-row/custom';
+            $scope.gridOptions.rowDoubleClick = $scope.gridOptions.rowDoubleClick || function ($event, row, scope) {
+                    $event.stopPropagation();
                 }
-            });
-            var pageSizeChangeFirst = false;//第一次事件忽略
-            $scope.$watch('gridOptions.paginationPageSize', function (newVal, oldVal) {
-                if ($scope.gridOptions.pageChange && pageSizeChangeFirst) {
-                    $scope.gridOptions.pageChange($scope.gridOptions.paginationPageSize, $scope.gridOptions.paginationCurrentPage);
-                }
-                pageSizeChangeFirst = true;
-            });
+            if ($scope.gridOptions.pageChange) {//值监听防止不兼容新的分页模式
+                $scope.$watch('gridOptions.paginationCurrentPage', function (newVal, oldVal) {
+                    if ($scope.gridOptions.pageChange) {
+                        $scope.gridOptions.pageChange($scope.gridOptions.paginationPageSize, $scope.gridOptions.paginationCurrentPage);
+                    }
+                });
+                var pageSizeChangeFirst = false;//第一次事件忽略
+                $scope.$watch('gridOptions.paginationPageSize', function (newVal, oldVal) {
+                    if ($scope.gridOptions.pageChange && pageSizeChangeFirst) {
+                        $scope.gridOptions.pageChange($scope.gridOptions.paginationPageSize, $scope.gridOptions.paginationCurrentPage);
+                    }
+                    pageSizeChangeFirst = true;
+                });
+            }
+            //////模板改造
             //复选框全选事件   注意  对象为  gridApi
             //    gridApi.selection.on.rowSelectionChangedBatch($scope, function (allRows) {
             //                 console.log(allRows)
@@ -110,7 +181,7 @@ angular.module('common.components').directive('normalGrid', function ($timeout, 
             // gridApi.selection.on.rowSelectionChanged($scope, function (row) {
             //     console.log(row.isSelected)
             //     console.log(row.entity)
-            // }); 
+            // });
             //$scope.gridOptions = {
             //    totalItems: 60,
             //    enableSorting: false,
@@ -134,7 +205,7 @@ angular.module('common.components').directive('normalGrid', function ($timeout, 
             //     $scope.clickMeParent = function () {
             //     alert('me3')
             // }
-            //gridOptions.columnDefs[0].enableColumnMenu 是否显示列头的菜单 
+            //gridOptions.columnDefs[0].enableColumnMenu 是否显示列头的菜单
             // $scope.gridOptions.columnDefs = [
             //     { field: 'type1', displayName: '咨询分类', width: 150 , enableColumnMenu: false},
             //     { field: 'type2', displayName: '二级分类', width: 150 },
@@ -150,7 +221,14 @@ angular.module('common.components').directive('normalGrid', function ($timeout, 
             //uigrid表格內图标按钮案例
             //<button class="btn btn-default grid-operation-btn-edit" ><span class="glyphicon glyphicon-edit"></span>编辑</button>
             //<button class="btn btn-default grid-operation-btn-del" ><span class="glyphicon glyphicon-remove"></span>删除</button>
+            //onRegisterApi: function (gridApi) {
 
+            //     gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
+
+            //         $scope.getQuestionsData({ pageIndex: newPage, pageSize: pageSize });
+
+            //     });
+            //}
         }
     };
 });
